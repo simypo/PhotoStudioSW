301df1aba99b5ea821b5277422d747b5372fcfdf
simypo <simypo@gmail.com>
2017-04-14 22:13:29 +0200

    versiunea 2
    rezolvata problema blocarii. pentru a citi valorile de pe serial while()
    a fost inlocuit cu if()

--- test/test.ino
+++ test/test.ino
@@ -6,22 +6,22 @@
 
  */
 
-// constants won't change. Used here to set a pin number :
+// constants used to set a pin number :
 const int ledPin = 13;// the number of the LED pin
 const int sensor = 8;
 // Variables will change :
 //int ledState = LOW;             // ledState used to set the LED
 int sensorState = LOW;
 
-// Generally, you should use "unsigned long" for variables that hold time
+// unsigned long for variables that hold time
 // The value will quickly become too large for an int to store
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
-// constants won't change :
-//const int val = 0;           // interval at which to blink (milliseconds)
+//this constant will be inserted from serial monitor
+int interval;           // interval at which to blink (milliseconds)
 
 void setup() {
-  // set the digital pin as output:
+  // set the digital pin as input or output:
   pinMode(ledPin, OUTPUT);
   pinMode(sensor, INPUT);
   Serial.begin(9600);
@@ -29,30 +29,34 @@ void setup() {
 
 void loop() {
   sensorState = digitalRead(sensor);
-  //Serial.println(sensorState);
+  
+ //program start if the sensor will detect something
   if(sensorState == LOW){
   Serial.println("program start");  
+  Serial.print("previous millis: ");
+  Serial.println(previousMillis);
   // here is where you'd put code that needs to be running all the time.
-
-    //waiting for input
-  while (Serial.available() == 0);
-  int val = Serial.parseInt(); //read int or parseFloat for ..float...
-    
-  Serial.print("seteaza intervalul pentru declansare:");
-  Serial.println(val);
   unsigned long currentMillis = millis();
   Serial.print("current millis: ");
   Serial.println(currentMillis);
-  if (currentMillis - previousMillis >= val) {
+    //waiting for input
+    if(Serial.available() > 0){
+    interval = Serial.parseInt();
+    }//read int or parseFloat for ..float... 
+  Serial.print("seteaza intervalul pentru declansare:");
+  Serial.println(interval);
+  
+  if (currentMillis - previousMillis >= interval) {
      digitalWrite(ledPin, HIGH);
+     Serial.println("declanseaza");
+     Serial.print("current millis: ");
+    Serial.println(currentMillis);
      delay(1000); 
      digitalWrite(ledPin, LOW);
     }
-    // set the LED with the ledState of the variable:
-    //digitalWrite(ledPin, ledState);
      // save the last time you blinked the LED
-    previousMillis = currentMillis;
-  Serial.print("previous millis: ");
+  previousMillis = currentMillis;
+  Serial.print("previous = current millis: ");
   Serial.println(previousMillis);
   }
 }
